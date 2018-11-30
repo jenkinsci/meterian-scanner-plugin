9228f31cd5e33bea715beb82f90fb1080245bda0
mani <sadhak001@gmail.com>
2018-11-30 14:58:51 +0000

    Break the build when meterian fails during a pipeline job

--- src/main/java/io/meterian/jenkins/glue/executors/StandardExecutor.java
+++ src/main/java/io/meterian/jenkins/glue/executors/StandardExecutor.java
@@ -1,17 +1,25 @@
 package io.meterian.jenkins.glue.executors;
 
+import hudson.model.Result;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 
 import io.meterian.jenkins.core.Meterian;
 
 public class StandardExecutor implements MeterianExecutor {
 
+    private StepContext context;
+
     public StandardExecutor(StepContext context) {
+        this.context = context;
     }
 
     @Override
     public void run(Meterian client) throws Exception {
-        client.run("--interactive=false");
+        Meterian.Result result = client.run("--interactive=false");
+
+        if (result.exitCode != 0) {
+            context.setResult(Result.FAILURE);
+        }
     }
 
 }

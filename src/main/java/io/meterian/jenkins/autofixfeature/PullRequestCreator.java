0ebeca490bfa8c5ed230f2234d778033bd649972
neomatrix369 <sadhak001@gmail.com>
2018-12-09 13:50:21 +0000

    Extracted the client runners of the two kinds into their own types and applied across other classes

--- src/main/java/io/meterian/jenkins/autofixfeature/PullRequestCreator.java
+++ src/main/java/io/meterian/jenkins/autofixfeature/PullRequestCreator.java
@@ -1,8 +1,8 @@
 package io.meterian.jenkins.autofixfeature;
 
-import io.meterian.jenkins.glue.ClientRunner;
 import io.meterian.jenkins.autofixfeature.git.LocalGitClient;
 import io.meterian.jenkins.autofixfeature.github.LocalGitHubClient;
+import io.meterian.jenkins.glue.ClientRunner;
 import io.meterian.jenkins.glue.MeterianPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/io/meterian/jenkins/glue/ClientRunner.java
+++ src/main/java/io/meterian/jenkins/glue/ClientRunner.java
@@ -1,52 +1,5 @@
 package io.meterian.jenkins.glue;
 
-import hudson.model.AbstractBuild;
-import hudson.model.Result;
-import io.meterian.jenkins.core.Meterian;
-import io.meterian.jenkins.glue.executors.MeterianExecutor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.PrintStream;
-
-public class ClientRunner {
-    static final Logger log = LoggerFactory.getLogger(ClientRunner.class);
-
-    private AbstractBuild build;
-    private Meterian client;
-    private MeterianExecutor executor;
-    private PrintStream jenkinsLogger;
-
-    public ClientRunner(AbstractBuild build,
-                        Meterian client,
-                        PrintStream jenkinsLogger) {
-        this.build = build;
-        this.client = client;
-        this.jenkinsLogger = jenkinsLogger;
-    }
-
-    public ClientRunner(MeterianExecutor executor, Meterian client,
-                        PrintStream jenkinsLogger) {
-        this.client = client;
-        this.executor = executor;
-        this.jenkinsLogger = jenkinsLogger;
-    }
-
-    public void execute() throws IOException {
-        if (executor == null) {
-            Meterian.Result buildResult = client.run();
-            if (buildResult.exitCode != 0) {
-                build.setResult(Result.FAILURE);
-            }
-        } else {
-            try {
-                executor.run(client);
-            } catch (Exception ex) {
-                log.warn("Unexpected", ex);
-                jenkinsLogger.println("Unexpected exception!");
-                ex.printStackTrace(jenkinsLogger);
-            }
-        }
-    }
+public interface ClientRunner {
+    void execute();
 }

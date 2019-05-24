b5d1e433f305682e1ddb50a3357169ddb43d5205
mani <sadhak001@gmail.com>
2019-05-24 14:20:45 +0100

    Final preparations to make the test work in remote/isolate environments, reading values off the environment, checking if the environment has the values set. Cloning repo cleanly each time, running client against the cloned repo.
    Finally verifying if the output from the meterian client analysis is as expected

--- src/test/java/integration_tests/MeterianClientRunnerTest.java
+++ src/test/java/integration_tests/MeterianClientRunnerTest.java
@@ -2,46 +2,84 @@ package integration_tests;
 
 import hudson.EnvVars;
 import hudson.slaves.EnvironmentVariablesNodeProperty;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.http.client.HttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import com.meterian.common.system.OS;
+import com.meterian.common.system.Shell;
 import io.meterian.jenkins.core.Meterian;
 import io.meterian.jenkins.glue.MeterianPlugin;
 import io.meterian.jenkins.io.ClientDownloader;
 import io.meterian.jenkins.io.HttpClientFactory;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.output.NullOutputStream;
-import org.apache.http.client.HttpClient;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
 
 import static junit.framework.TestCase.fail;
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class MeterianClientRunnerTest {
 
-    private static final String CURRENT_WORKING_DIR = System.getProperty("user.dir");
     private static final String BASE_URL = "https://www.meterian.com";
+    private static final String CURRENT_WORKING_DIR = System.getProperty("user.dir");
 
-    private String jenkinsProjectName = "TestMeterianPlugin-Freestyle-autofix";
+    private static final String NO_JVM_ARGS = "";
+
+    private String gitRepoWorkingFolder;
+
+    @Before
+    public void setup() throws IOException {
+        String gitRepoRootFolder = Paths.get(CURRENT_WORKING_DIR, "target/github-repo/").toString();
+        FileUtils.deleteDirectory(new File(gitRepoRootFolder));
+
+        new File(gitRepoRootFolder).mkdir();
+
+        gitRepoWorkingFolder = performCloneGitRepo(gitRepoRootFolder);
+    }
+
+    private String performCloneGitRepo(String gitRepoRootFolder) throws IOException {
+        String githubProjectName = "autofix-sample-maven-upgrade";
+        String[] gitCloneRepoCommand = new String[] {
+          "git",
+          "clone",
+          "https://github.com/MeterianHQ/" + githubProjectName + ".git"
+        };
+
+        Shell.Options options = new Shell.Options().
+                onDirectory(new File(gitRepoRootFolder));
+        Shell.Task task = new Shell().exec(
+                gitCloneRepoCommand,
+                options
+        );
+        task.waitFor();
+
+        assertThat("Cannot run the test, as we were unable to clone the target git repo due to error code: " +
+                task.exitValue(), task.exitValue(), is(equalTo(0)));
+
+        return Paths.get(gitRepoRootFolder, githubProjectName).toString();
+    }
 
     @Test
-//    @Ignore("Runs only in an environment with the Jenkins hpi and Meterian setup in place")
-    public void givenConfiguration_whenMeterianClientIsRun_thenItShouldNotThrowException()
-            throws IOException, ParserConfigurationException, SAXException {
-        EnvVars environment = getEnvironment(jenkinsProjectName);
+    public void givenConfiguration_whenMeterianClientIsRun_thenItShouldNotThrowException() throws IOException {
+        // Given: we are setup to run the meterian client against a repo that has vulnerabilities
+
+        EnvVars environment = getEnvironment();
+        String meterianAPIToken = environment.get("METERIAN_API_TOKEN");
+        assertThat("METERIAN_API_TOKEN has not been set, cannot run test without a valid value", meterianAPIToken, notNullValue());
+        String meterianGithubToken = environment.get("METERIAN_GITHUB_TOKEN");
+        assertThat("METERIAN_GITHUB_TOKEN has not been set, cannot run test without a valid value", meterianGithubToken, notNullValue());
+
         MeterianPlugin.Configuration configuration = new MeterianPlugin.Configuration(
                 BASE_URL,
-                environment.get("METERIAN_API_TOKEN"),
-                "", // jvmArgs
-                environment.get("METERIAN_GITHUB_TOKEN")
+                meterianAPIToken,
+                NO_JVM_ARGS,
+                meterianGithubToken
         );
 
         File logFile = File.createTempFile("jenkins-logger", Long.toString(System.nanoTime()));
@@ -49,21 +87,27 @@ public class MeterianClientRunnerTest {
 
         String args = "";
 
+        // When: the meterian client is run against the locally cloned git repo
         try {
             File clientJar = new ClientDownloader(newHttpClient(), BASE_URL, nullPrintStream()).load();
             Meterian client = Meterian.build(configuration, environment, jenkinsLogger, args, clientJar);
             client.prepare("--interactive=false");
             client.run();
             jenkinsLogger.close();
-
-            String buildLogs = readBuildLogs(logFile.getPath());
-            assertThat(buildLogs, containsString("[meterian] Client successfully authorized"));
-            assertThat(buildLogs, containsString("[meterian] Meterian Client v"));
-            assertThat(buildLogs, containsString("[meterian] Project information:"));
-            assertThat(buildLogs, containsString("[meterian] JAVA scan -"));
         } catch (Exception ex) {
             fail("Should not have failed with the exception: " + ex.getMessage());
         }
+
+        // Then: we should be able to see the expecting output in the execution analysis output logs
+        String runAnalysisLogs = readBuildLogs(logFile.getPath());
+        assertThat(runAnalysisLogs, containsString("[meterian] Client successfully authorized"));
+        assertThat(runAnalysisLogs, containsString("[meterian] Meterian Client v"));
+        assertThat(runAnalysisLogs, containsString("[meterian] Project information:"));
+        assertThat(runAnalysisLogs, containsString("[meterian] JAVA scan -"));
+        assertThat(runAnalysisLogs, containsString("MeterianHQ/autofix-sample-maven-upgrade.git"));
+        assertThat(runAnalysisLogs, containsString("[meterian] Full report available at: "));
+        assertThat(runAnalysisLogs, containsString("[meterian] Build unsuccesful!"));
+        assertThat(runAnalysisLogs, containsString("[meterian] Failed checks: [security]"));
     }
 
     private String readBuildLogs(String pathToBuildLog) throws IOException {
@@ -104,28 +148,15 @@ public class MeterianClientRunnerTest {
             }});
     }
 
-    private EnvVars getEnvironment(String projectName) throws IOException, SAXException, ParserConfigurationException {
+    private EnvVars getEnvironment() {
         EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
         EnvVars environment = prop.getEnvVars();
-        environment.put("WORKSPACE", getPathToWorkspace(projectName));
 
-        String pathToMeterianPluginConfig = CURRENT_WORKING_DIR + "/work/io.meterian.jenkins.glue.MeterianPlugin.xml";
-        Document xmlDocument = loadXMLFile(pathToMeterianPluginConfig);
-        environment.put("METERIAN_API_TOKEN", xmlDocument.getElementsByTagName("token").item(0).getTextContent());
-        environment.put("METERIAN_GITHUB_TOKEN", xmlDocument.getElementsByTagName("githubToken").item(0).getTextContent());
+        Map<String, String> localEnvironment = new OS().getenv();
+        for (String envKey: localEnvironment.keySet()) {
+            environment.put(envKey, localEnvironment.get(envKey));
+        }
+        environment.put("WORKSPACE", gitRepoWorkingFolder);
         return environment;
-    }
-
-    private Document loadXMLFile(String filename) throws ParserConfigurationException, IOException, SAXException {
-        File file = new File(filename);
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        Document document = db.parse(file);
-        document.getDocumentElement().normalize();
-        return document;
-    }
-
-    private String getPathToWorkspace(String projectName) {
-        return String.format("%s/work/workspace/%s", CURRENT_WORKING_DIR, projectName);
     }
 }
